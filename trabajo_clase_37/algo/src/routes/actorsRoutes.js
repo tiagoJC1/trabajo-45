5184e3a3d02cb58f7a6321492f375ffbc603fd8c
Tiago Joaquín Cuello <tiagocuello679@gmail.com>
2024-07-09 20:03:21 -0300

    Delete

--- trabajo_clase_37/algo/src/routes/actorsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const actorsController = require('../controllers/actorsController');
-
-router.get('/actors', actorsController.list);
-router.get('/actors/detail/:id', actorsController.detail);
-
-
-module.exports = router;
